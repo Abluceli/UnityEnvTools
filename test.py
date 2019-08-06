7daced773e9330f9c53bf013049f84d1454983c9
787873309@qq.com <liyang@liyangdeMacBook-Pro.local>
2019-08-06 20:11:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from UnityEnv import UnityEnv
 from mlagents.envs import UnityEnvironment
 
 if __name__ == "__main__":
-    env = UnityEnvironment(file_name='/Users/liyang/Code/UnityEnv/Soccer', worker_id=0, no_graphics=False)
+    env = UnityEnvironment(file_name='/Users/liyang/Code/UnityEnv/Soccer', worker_id=0, no_graphics=True)
     # #env = UnityEnvironment(file_name='UnityEnvTools/soccer/soccer', worker_id=1, no_graphics=True)
     # print(env.action_space)
     # print(env.observation_space)
