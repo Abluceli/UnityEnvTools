4cddbea7cd5ca964e4aa7df1365e7c87f5b9ee8a
787873309@qq.com <liyang@liyangdeMacBook-Pro.local>
2019-08-06 20:07:57 +0800

    reset no done

--- UnityEnv.py
+++ UnityEnv.py
@@ -65,71 +65,60 @@ class UnityEnv(gym.Env):
             raise UnityGymException(
                 "There are not any external brain in the UnityEnvironment"
             )
-        
-        
 
-    
-
-        # if brain.number_visual_observations > 1 and not self._allow_multiple_visual_obs:
-        #     logger.warning(
-        #         "The environment contains more than one visual observation. "
-        #         "You must define allow_multiple_visual_obs=True to received them all. "
-        #         "Otherwise, please note that only the first will be provided in the observation."
-        #     )
-
-        # if brain.num_stacked_vector_observations != 1:
-        #     raise UnityGymException(
-        #         "There can only be one stacked vector observation in a UnityEnvironment "
-        #         "if it is wrapped in a gym."
-        #     )
-
-        # Check for number of agents in scene.
-        initial_info = self._env.reset()[self.brain_name]
-        self._check_agents(len(initial_info.agents))
+        # # Check for number of agents in scene.
+        # initial_info = self._env.reset()
+        # self._check_agents(len(initial_info.agents))
 
 
-        self.
+        self.action_space = {}
+        self.observation_space = {}
+
+
         # Set observation and action spaces
-        if brain.vector_action_space_type == "discrete":
-            if len(brain.vector_action_space_size) == 1:
-                self._action_space = spaces.Discrete(brain.vector_action_space_size[0])
+        for brain_name in self.brain_names:
+            brain = self.brains[brain_name]
+            if brain.vector_action_space_type == "discrete":
+                if len(brain.vector_action_space_size) == 1:
+                    self.action_space[brain_name] = spaces.Discrete(brain.vector_action_space_size[0])
+                else:
+                    # if flatten_branched:
+                    #     self._flattener = ActionFlattener(brain.vector_action_space_size)
+                    #     self._action_space = self._flattener.action_space
+                    # else:
+                    #     self._action_space = spaces.MultiDiscrete(
+                    #         brain.vector_action_space_size
+                    #     )
+                    pass
+
             else:
                 if flatten_branched:
-                    self._flattener = ActionFlattener(brain.vector_action_space_size)
-                    self._action_space = self._flattener.action_space
-                else:
-                    self._action_space = spaces.MultiDiscrete(
-                        brain.vector_action_space_size
+                    logger.warning(
+                        "The environment has a non-discrete action space. It will "
+                        "not be flattened."
                     )
-
-        else:
-            if flatten_branched:
-                logger.warning(
-                    "The environment has a non-discrete action space. It will "
-                    "not be flattened."
+                high = np.array([1] * brain.vector_action_space_size[0])
+                self.action_space[brain_name] = spaces.Box(-high, high, dtype=np.float32)
+            #np.inf mean the max number in python
+            high = np.array([np.inf] * brain.vector_observation_space_size)
+            self.action_meanings = brain.vector_action_descriptions
+            if use_visual:
+                if brain.camera_resolutions[0]["blackAndWhite"]:
+                    depth = 1
+                else:
+                    depth = 3
+                self._observation_space = spaces.Box(
+                    0,
+                    1,
+                    dtype=np.float32,
+                    shape=(
+                        brain.camera_resolutions[0]["height"],
+                        brain.camera_resolutions[0]["width"],
+                        depth,
+                    ),
                 )
-            high = np.array([1] * brain.vector_action_space_size[0])
-            self._action_space = spaces.Box(-high, high, dtype=np.float32)
-
-        high = np.array([np.inf] * brain.vector_observation_space_size)
-        self.action_meanings = brain.vector_action_descriptions
-        if self.use_visual:
-            if brain.camera_resolutions[0]["blackAndWhite"]:
-                depth = 1
             else:
-                depth = 3
-            self._observation_space = spaces.Box(
-                0,
-                1,
-                dtype=np.float32,
-                shape=(
-                    brain.camera_resolutions[0]["height"],
-                    brain.camera_resolutions[0]["width"],
-                    depth,
-                ),
-            )
-        else:
-            self._observation_space = spaces.Box(-high, high, dtype=np.float32)
+                self.observation_space[brain_name] = spaces.Box(-high, high, dtype=np.float32)
 
     def reset(self):
         """Resets the state of the environment and returns an initial observation.
@@ -137,120 +126,119 @@ class UnityEnv(gym.Env):
         Returns: observation (object/list): the initial observation of the
             space.
         """
-        info = self._env.reset()[self.brain_name]
-        n_agents = len(info.agents)
-        self._check_agents(n_agents)
-        self.game_over = False
-
-        if not self._multiagent:
-            obs, reward, done, info = self._single_step(info)
-        else:
-            obs, reward, done, info = self._multi_step(info)
+        print("start")
+        info_all = self._env.reset()
+        print("end")
+        obs = {}
+        for brain_name in self.brain_names:
+            
+            brain_info = info_all[brain_name]
+            obs[brain_name] = brain_info.vector_observations
         return obs
 
-    def step(self, action):
-        """Run one timestep of the environment's dynamics. When end of
-        episode is reached, you are responsible for calling `reset()`
-        to reset this environment's state.
-        Accepts an action and returns a tuple (observation, reward, done, info).
-        In the case of multi-agent environments, these are lists.
-        Args:
-            action (object/list): an action provided by the environment
-        Returns:
-            observation (object/list): agent's observation of the current environment
-            reward (float/list) : amount of reward returned after previous action
-            done (boolean/list): whether the episode has ended.
-            info (dict): contains auxiliary diagnostic information, including BrainInfo.
-        """
+#     def step(self, action):
+#         """Run one timestep of the environment's dynamics. When end of
+#         episode is reached, you are responsible for calling `reset()`
+#         to reset this environment's state.
+#         Accepts an action and returns a tuple (observation, reward, done, info).
+#         In the case of multi-agent environments, these are lists.
+#         Args:
+#             action (object/list): an action provided by the environment
+#         Returns:
+#             observation (object/list): agent's observation of the current environment
+#             reward (float/list) : amount of reward returned after previous action
+#             done (boolean/list): whether the episode has ended.
+#             info (dict): contains auxiliary diagnostic information, including BrainInfo.
+#         """
 
-        # Use random actions for all other agents in environment.
-        if self._multiagent:
-            if not isinstance(action, list):
-                raise UnityGymException(
-                    "The environment was expecting `action` to be a list."
-                )
-            if len(action) != self._n_agents:
-                raise UnityGymException(
-                    "The environment was expecting a list of {} actions.".format(
-                        self._n_agents
-                    )
-                )
-            else:
-                if self._flattener is not None:
-                    # Action space is discrete and flattened - we expect a list of scalars
-                    action = [self._flattener.lookup_action(_act) for _act in action]
-                action = np.array(action)
-        else:
-            if self._flattener is not None:
-                # Translate action into list
-                action = self._flattener.lookup_action(action)
+#         # Use random actions for all other agents in environment.
+#         if self._multiagent:
+#             if not isinstance(action, list):
+#                 raise UnityGymException(
+#                     "The environment was expecting `action` to be a list."
+#                 )
+#             if len(action) != self._n_agents:
+#                 raise UnityGymException(
+#                     "The environment was expecting a list of {} actions.".format(
+#                         self._n_agents
+#                     )
+#                 )
+#             else:
+#                 if self._flattener is not None:
+#                     # Action space is discrete and flattened - we expect a list of scalars
+#                     action = [self._flattener.lookup_action(_act) for _act in action]
+#                 action = np.array(action)
+#         else:
+#             if self._flattener is not None:
+#                 # Translate action into list
+#                 action = self._flattener.lookup_action(action)
 
-        info = self._env.step(action)[self.brain_name]
-        n_agents = len(info.agents)
-        self._check_agents(n_agents)
-        self._current_state = info
+#         info = self._env.step(action)[self.brain_name]
+#         n_agents = len(info.agents)
+#         self._check_agents(n_agents)
+#         self._current_state = info
 
-        if not self._multiagent:
-            obs, reward, done, info = self._single_step(info)
-            self.game_over = done
-        else:
-            obs, reward, done, info = self._multi_step(info)
-            self.game_over = all(done)
-        return obs, reward, done, info
+#         if not self._multiagent:
+#             obs, reward, done, info = self._single_step(info)
+#             self.game_over = done
+#         else:
+#             obs, reward, done, info = self._multi_step(info)
+#             self.game_over = all(done)
+#         return obs, reward, done, info
 
-    def _single_step(self, info):
-        if self.use_visual:
-            visual_obs = info.visual_observations
+#     def _single_step(self, info):
+#         if self.use_visual:
+#             visual_obs = info.visual_observations
 
-            if self._allow_multiple_visual_obs:
-                visual_obs_list = []
-                for obs in visual_obs:
-                    visual_obs_list.append(self._preprocess_single(obs[0]))
-                self.visual_obs = visual_obs_list
-            else:
-                self.visual_obs = self._preprocess_single(visual_obs[0][0])
+#             if self._allow_multiple_visual_obs:
+#                 visual_obs_list = []
+#                 for obs in visual_obs:
+#                     visual_obs_list.append(self._preprocess_single(obs[0]))
+#                 self.visual_obs = visual_obs_list
+#             else:
+#                 self.visual_obs = self._preprocess_single(visual_obs[0][0])
 
-            default_observation = self.visual_obs
-        else:
-            default_observation = info.vector_observations[0, :]
+#             default_observation = self.visual_obs
+#         else:
+#             default_observation = info.vector_observations[0, :]
 
-        return (
-            default_observation,
-            info.rewards[0],
-            info.local_done[0],
-            {"text_observation": info.text_observations[0], "brain_info": info},
-        )
+#         return (
+#             default_observation,
+#             info.rewards[0],
+#             info.local_done[0],
+#             {"text_observation": info.text_observations[0], "brain_info": info},
+#         )
 
-    def _preprocess_single(self, single_visual_obs):
-        if self.uint8_visual:
-            return (255.0 * single_visual_obs).astype(np.uint8)
-        else:
-            return single_visual_obs
+#     def _preprocess_single(self, single_visual_obs):
+#         if self.uint8_visual:
+#             return (255.0 * single_visual_obs).astype(np.uint8)
+#         else:
+#             return single_visual_obs
 
-    def _multi_step(self, info):
-        if self.use_visual:
-            self.visual_obs = self._preprocess_multi(info.visual_observations)
-            default_observation = self.visual_obs
-        else:
-            default_observation = info.vector_observations
-        return (
-            list(default_observation),
-            info.rewards,
-            info.local_done,
-            {"text_observation": info.text_observations, "brain_info": info},
-        )
+#     def _multi_step(self, info):
+#         if self.use_visual:
+#             self.visual_obs = self._preprocess_multi(info.visual_observations)
+#             default_observation = self.visual_obs
+#         else:
+#             default_observation = info.vector_observations
+#         return (
+#             list(default_observation),
+#             info.rewards,
+#             info.local_done,
+#             {"text_observation": info.text_observations, "brain_info": info},
+#         )
 
-    def _preprocess_multi(self, multiple_visual_obs):
-        if self.uint8_visual:
-            return [
-                (255.0 * _visual_obs).astype(np.uint8)
-                for _visual_obs in multiple_visual_obs
-            ]
-        else:
-            return multiple_visual_obs
+#     def _preprocess_multi(self, multiple_visual_obs):
+#         if self.uint8_visual:
+#             return [
+#                 (255.0 * _visual_obs).astype(np.uint8)
+#                 for _visual_obs in multiple_visual_obs
+#             ]
+#         else:
+#             return multiple_visual_obs
 
-    def render(self, mode="rgb_array"):
-        return self.visual_obs
+#     def render(self, mode="rgb_array"):
+#         return self.visual_obs
 
     def close(self):
         """Override _close in your subclass to perform any necessary cleanup.
@@ -259,66 +247,65 @@ class UnityEnv(gym.Env):
         """
         self._env.close()
 
-    def get_action_meanings(self):
-        return self.action_meanings
+#     def get_action_meanings(self):
+#         return self.action_meanings
 
-    def seed(self, seed=None):
-        """Sets the seed for this env's random number generator(s).
-        Currently not implemented.
-        """
-        logger.warn("Could not seed environment %s", self.name)
-        return
+#     def seed(self, seed=None):
+#         """Sets the seed for this env's random number generator(s).
+#         Currently not implemented.
+#         """
+#         logger.warn("Could not seed environment %s", self.name)
+#         return
 
-    def _check_agents(self, n_agents):
-        if not self._multiagent and n_agents > 1:
-            raise UnityGymException(
-                "The environment was launched as a single-agent environment, however"
-                "there is more than one agent in the scene."
-            )
-        elif self._multiagent and n_agents <= 1:
-            raise UnityGymException(
-                "The environment was launched as a mutli-agent environment, however"
-                "there is only one agent in the scene."
-            )
-        if self._n_agents is None:
-            self._n_agents = n_agents
-            logger.info("{} agents within environment.".format(n_agents))
-        elif self._n_agents != n_agents:
-            raise UnityGymException(
-                "The number of agents in the environment has changed since "
-                "initialization. This is not supported."
-            )
+#     def _check_agents(self, n_agents):
+#         if not self._multiagent and n_agents > 1:
+#             raise UnityGymException(
+#                 "The environment was launched as a single-agent environment, however"
+#                 "there is more than one agent in the scene."
+#             )
+#         elif self._multiagent and n_agents <= 1:
+#             raise UnityGymException(
+#                 "The environment was launched as a mutli-agent environment, however"
+#                 "there is only one agent in the scene."
+#             )
+#         if self._n_agents is None:
+#             self._n_agents = n_agents
+#             logger.info("{} agents within environment.".format(n_agents))
+#         elif self._n_agents != n_agents:
+#             raise UnityGymException(
+#                 "The number of agents in the environment has changed since "
+#                 "initialization. This is not supported."
+#             )
 
-    @property
-    def metadata(self):
-        return {"render.modes": ["rgb_array"]}
+#     @property
+#     def metadata(self):
+#         return {"render.modes": ["rgb_array"]}
 
-    @property
-    def reward_range(self):
-        return -float("inf"), float("inf")
+#     @property
+#     def reward_range(self):
+#         return -float("inf"), float("inf")
 
-    @property
-    def spec(self):
-        return None
+#     @property
+#     def spec(self):
+#         return None
 
-    @property
-    def action_space(self):
-        return self._action_space
+#     @property
+#     def action_space(self):
+#         return self._action_space
 
-    @property
-    def observation_space(self):
-        return self._observation_space
+#     @property
+#     def observation_space(self):
+#         return self._observation_space
 
-    @property
-    def number_agents(self):
-        return self._n_agents
+#     @property
+#     def number_agents(self):
+#         return self._n_agents
 
 
 class ActionFlattener:
     """
     Flattens branched discrete action spaces into single-branch discrete action spaces.
     """
-
     def __init__(self, branched_action_space):
         """
         Initialize the flattener.
--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 from UnityEnv import UnityEnv
 from mlagents.envs import UnityEnvironment
+
 if __name__ == "__main__":
-    env = UnityEnv(environment_filename='UnityEnvTools/soccer/soccer', worker_id=0, no_graphics=True)
-    #env = UnityEnvironment(file_name='UnityEnvTools/soccer/soccer', worker_id=1, no_graphics=True)
-    print(env.action_space)
-    print(env.observation_space)
-    print(env.brain_name)
-	"""""""""
+    env = UnityEnvironment(file_name='/Users/liyang/Code/UnityEnv/Soccer', worker_id=0, no_graphics=False)
+    # #env = UnityEnvironment(file_name='UnityEnvTools/soccer/soccer', worker_id=1, no_graphics=True)
+    # print(env.action_space)
+    # print(env.observation_space)
+    # print(env.brain_names)
+    # print(env.brains)
+    # print(env.observation_space)
+    # print(env.action_space)
+    # obs = env.reset()
+    # print(obs)
+    # env.close()
+    info = env.reset()
+    env.close
